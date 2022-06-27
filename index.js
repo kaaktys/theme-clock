688965cf1f34f0dec210d3d8af0cbb9afbbe6503
Kate <katok0709@gmail.com>
2022-06-28 00:22:12 +0300

    html + consts

---
+++ index.js
@@ -0,0 +1,7 @@
+const hourEl = document.querySelector('.hour')
+const minuteEl = document.querySelector('.minute')
+const secondEl = document.querySelector('.second')
+const timeEl = document.querySelector('.time')
+const dateEl = document.querySelector('.date')
+const btn = document.querySelector('.btn')
+
